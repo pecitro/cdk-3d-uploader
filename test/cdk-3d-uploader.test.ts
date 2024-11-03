91e3f254a8a3d1b7f87d5ecb878b5df8cb7d048a
yuta.odagawa <hoge@example.com>
2024-11-03 16:05:19 +0900

    Initial commit

---
+++ test/cdk-3d-uploader.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Cdk3DUploader from '../lib/cdk-3d-uploader-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-3d-uploader-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Cdk3DUploader.Cdk3DUploaderStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
